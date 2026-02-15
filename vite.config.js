e9421542f147fe329d30888c3544452ad4f2a367
Miraitowa-00 <2475657731@qq.com>
2026-02-15 18:25:40 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    open: true
+  base: '/caidan/',  // 确保相对路径以 /caidan/ 为基础
+  build: {
+    outDir: 'dist',  // 输出目录
+    assetsDir: 'assets', // 静态资源目录
   }
 })
-    
